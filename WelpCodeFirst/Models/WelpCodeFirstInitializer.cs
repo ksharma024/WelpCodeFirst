2f7826dab29c14f822a32dae83e11a5f883ffd72
Karn Sharma <karn.sharma024@gmail.com>
2016-07-26 11:34:02 -0400

    Added Seed

--- WelpCodeFirst/Models/WelpCodeFirstInitializer.cs
+++ WelpCodeFirst/Models/WelpCodeFirstInitializer.cs
@@ -6,8 +6,16 @@ using System.Web;
 namespace WelpCodeFirst.Models
 {
     public class WelpCodeFirstInitializer : System.Data.Entity.DropCreateDatabaseIfModelChanges<WelpCodeFirstContext>
+        
+        //initializers can be used to seed the database. SEEDING means everytime model chagned the changes are made but then the
+        //seed method is run to drop database, recreate it and put the new things (listed below) into the database.
 
         
     {
+        protected override void Seed(WelpCodeFirstContext context)
+        {
+            context.Users.Add(new User() { UserName = "johndoe", Email = "johndoe@gmail.com", Password = "llama" });
+            base.Seed(context);
+        }
     }
 }
